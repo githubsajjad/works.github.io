0ff5151d50894b8f93adc010e4be3b86e91034fa
sajjad <md.sajjadali23@gmail.com>
2017-06-20 17:39:29 +0600

    course-1 class-8

---
+++ course-1/class-eight/assets/js/active.js
@@ -0,0 +1,60 @@
+(function($){
+    "use strict";
+    
+    
+//     document ready 
+    jQuery(document).ready(function($){
+        
+	 $('#home-page-slides').owlCarousel({
+		nav: true,
+		items: 1,
+		loop: true,
+		center: true,
+		margin: 0,
+		lazyLoad:true,
+		dots: true,
+        navText: ["<i class='zmdi zmdi-long-arrow-left'></i>","<i class='zmdi zmdi-long-arrow-right'></i>"],
+	});
+        
+      $("#case-studies-carousel").owlCarousel({
+          items: 3,
+          margin: 30,
+          loop: true,
+          dots: true,
+          nav: false,
+          autoplay: true,
+      });
+        
+      $("#logo-carousel").owlCarousel({
+          items: 6,
+          margin: 30,
+          loop: true,
+          dots: false,
+          nav: false,
+          autoplay: true,
+      });
+        
+      $("#testimonial-carousel,#single-testimonial-carousel-theme-2").owlCarousel({
+          items: 1,
+          loop: true,
+          dots: true,
+          nav: false,
+        navText: ['<i class="fa fa-angle-double-left"></i>', '<i class="fa fa-angle-double-right"></i>'],
+      });  
+        
+
+});
+
+    
+ 
+
+    
+    
+    
+    
+    
+    
+    
+    
+    
+})(jQuery);
